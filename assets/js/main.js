18ac70f89e81eb72dfedd44253c9d50356292c8e
doyo91 <doyo_91@hotmail.com>
2020-09-17 13:29:00 +0200

    First commit

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// dark mode
+// const btn = document.getElementById("switch");
+// const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+
+// const currentTheme = localStorage.getItem("theme");
+// if (currentTheme == "dark") {
+//   document.body.classList.toggle("dark-theme");
+// } else if (currentTheme == "light") {
+//   document.body.classList.toggle("light-theme");
+// }
+
+// btn.addEventListener("click", function () {
+//   if (prefersDarkScheme.matches) {
+//     // document.body.classList.toggle("light-theme");
+//     var theme = document.body.classList.contains("light-theme")
+//       ? "light"
+//       : "dark";
+//   } else {
+//     // document.body.classList.toggle("dark-theme");
+//     var theme = document.body.classList.contains("dark-theme")
+//       ? "dark"
+//       : "light";
+//   }
+//   localStorage.setItem("theme", theme);
+// });
+
+// SAL JS
+sal();
