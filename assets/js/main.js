a34e6291d35978ecad2ab1428e4009a30665faa5
doyo91 <doyo_91@hotmail.com>
2020-09-17 19:15:31 +0200

    Header change

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,5 +24,25 @@
 //   localStorage.setItem("theme", theme);
 // });
 
+// Dia descripción
+const days = [
+  "domingo",
+  "lunes",
+  "martes",
+  "miércoles",
+  "jueves",
+  "viernes",
+  "sábado",
+];
+
+let currentDay = new Date().getDay();
+document.getElementById("day").innerHTML = days[currentDay];
+
+// Año footer
+let currentYear = new Date().getFullYear();
+document.getElementsByClassName(
+  "footer__currentYear"
+)[0].innerHTML = currentYear;
+
 // SAL JS
 sal();
