f28042d3863b45b9b37bed0568826678b600509e
doyo91 <doyo_91@hotmail.com>
2020-09-22 12:01:53 +0200

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require("gulp");
+const imagemin = require("gulp-imagemin");
+const terser = require("gulp-terser"); // recomendado ES6 minify
+const concat = require("gulp-concat");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+const { src, series, parallel, dest, watch } = require("gulp");
+
+const jsPath = "src/assets/js/**/*.js";
+const cssPath = "src/assets/css/**/*.css";
+
+function copyHtml() {
+  return src("src/*.html").pipe(gulp.dest("dist"));
+}
+
+function imgTask() {
+  return src("src/assets/img/*")
+    .pipe(imagemin())
+    .pipe(gulp.dest("dist/assets/images"));
+}
+
+function copyDocs() {
+  return src("src/assets/docs/*").pipe(gulp.dest("dist/assets/docs"));
+}
+
+function jsTask() {
+  return src(jsPath)
+    .pipe(sourcemaps.init())
+    .pipe(concat("main.min.js"))
+    .pipe(terser())
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("dist/assets/js"));
+}
+
+function cssTask() {
+  return src(cssPath)
+    .pipe(sourcemaps.init())
+    .pipe(concat("style.min.css"))
+    .pipe(postcss([autoprefixer(), cssnano()]))
+    .pipe(sourcemaps.write("."))
+    .pipe(dest("dist/assets/css"));
+}
+
+function watchTask() {
+  watch([cssPath, jsPath], { interval: 1000 }, parallel(cssTask, jsTask));
+}
+
+exports.cssTask = cssTask;
+exports.jsTask = jsTask;
+exports.imgTask = imgTask;
+exports.copyDocs = copyDocs;
+exports.copyHtml = copyHtml;
+exports.default = series(
+  parallel(copyHtml, copyDocs, imgTask, jsTask, cssTask),
+  watchTask
+);
